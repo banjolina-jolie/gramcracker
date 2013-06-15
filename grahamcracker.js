46474ae82acaa2ac5fc509f6134398de1329f07a
Dylan O'Carroll <dylanocarroll@Dylans-Laptop.local>
2013-06-15 15:27:03 -0700

    twerkin

--- grahamcracker.js
+++ grahamcracker.js
@@ -1,19 +1,38 @@
+Players = new Meteor.Collection("players");
+Hashtags = new Meteor.Collection("hashtags");
+Images = new Meteor.Collection("images");
+
 if (Meteor.isClient) {
+
+  Session.set("username", "Instagram Handle");
+
   Template.hello.greeting = function () {
-    return "Welcome to grahamcracker.";
+    if(Players.find().count() === 4){
+      return "Join a game!";
+    }
   };
 
   Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+    'click input#join' : function () {
+      Players.insert({username: Session.get("username"), thumbnail:"imageChoice", score: 0});
+    },
+    'click button#kill' : function () {
+      // Players.remove();
+      console.log(Players.find());
     }
   });
+
+  Template.judge.playerJudge = function() {
+    return Session.get("judge");
+  };
+
+  Template.players.list = function(){
+    return Players.find();
+  };
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    Players.remove({});
   });
 }
